857ae79d4ef93335ffaff25edeb22382d616dbd6
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-05-24 15:27:08 +0800

    refactor: refactor using `newVirtualThreadPerTaskExecutor`

--- src/main/java/com/ncku_csie_union/resources/Executor.java
+++ src/main/java/com/ncku_csie_union/resources/Executor.java
@@ -1,64 +1,60 @@
 package com.ncku_csie_union.resources;
 // import java.util.List;
 // import java.lang.Thread;
-import java.util.*; 
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.ncku_csie_union.resources.RateLimit;
 import com.ncku_csie_union.resources.interfaces.IExecutor;
 
 public class Executor extends Base implements IExecutor {
     private Integer VUs;
-    private Thread.Builder[] threadBuilders;
-    private Thread[] threads;
     private String name;
+    private Boolean stop;
+    private RateLimit rateLimit;
     public static Integer ExecutorCount = 0;
+    
 
     public Executor(Integer VUserCount) {
         this.logger.Log("[Executor] Constructor called");
-        this.name = "Executor-" + ExecutorCount++;
         this.VUs = VUserCount;
+        this.name = "Executor-" + ExecutorCount++;
+        this.stop = false;
+        this.rateLimit = new RateLimit(config.rate,config.rate);
         this.logger.Log("[Executor] VUs: " + this.VUs);
     }
     public void Init() {
         this.logger.Log("[Executor] Init called");
-        this.threadBuilders = new Thread.Builder[this.VUs];
-        for (int i = 0; i < this.VUs; i++) {
-            this.logger.Log("[Executor] Creating thread " + i);
-            threadBuilders[i] = Thread.ofVirtual().name(this.name + "-Thread-" + i);
-        }
     }
     public void Execute() {
-        this.logger.Log("[Executor] Execute called");
-        this.threads = new Thread[this.VUs];
-        for (int i = 0; i < this.VUs; i++) {
-            this.logger.Log("[Executor] Starting thread " + i);
-            Runnable task = () -> { 
-                while(true){
-                    System.out.println("Running VT" + Thread.currentThread().getName());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }; 
-            this.threads[i] = threadBuilders[i].start(task);
-        }
-        this.logger.Log("[Executor] All threads started");
-        this.logger.Log("[Executor] Waiting for threads to finish");
-        for (int i = 0; i < this.VUs; i++) {
+        this.logger.Log("[Executor:Execute] called");
+        Runnable mockTask = () -> { 
+            System.out.println("=========== Running VT:" + Thread.currentThread().getName() + " Start ===========");
+            System.out.println("[x] " + Thread.currentThread() + "...");
+
             try {
-                this.threads[i].join();
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
-                this.logger.Log("[Executor] Thread " + i + " interrupted");
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("=========== Running VT:" + Thread.currentThread().getName() + " Done ===========");
+        }; 
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            while(!this.stop) {
+                this.rateLimit.GetToken();
+                executor.submit(mockTask);
             }
         }
+        finally {
+            System.out.println("ExecutorService is shutdown");
+        }
+        this.logger.Log("[Executor:Execute] end");
     }
     public void Stop() {
-        this.logger.Log("[Executor] Stop called");
-        for (int i = 0; i < this.VUs; i++) {
-            this.logger.Log("[Executor] Stopping thread " + i);
-            this.threads[i].interrupt();
-        }
-        this.logger.Log("[Executor] All threads stopped");
+        this.logger.Log("[Executor:Stop] called");
+        this.stop = true;
+        this.logger.Log("[Executor:Stop] end");
     }
 }
