d2e1ae676f9b1f0677e4a95f577141ef055c6551
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-05-24 16:04:43 +0800

    feat: add dispatcher

--- src/main/java/com/ncku_csie_union/resources/Dispatcher.java
+++ src/main/java/com/ncku_csie_union/resources/Dispatcher.java
@@ -1,2 +1,35 @@
 package com.ncku_csie_union.resources;
 import com.ncku_csie_union.resources.interfaces.IDispatcher;
+
+public class Dispatcher extends Base implements IDispatcher {
+    private String logPrefix = "[Dispatcher]";
+    private Executor[] executors;
+    private Integer sharding;
+
+    public Dispatcher() {
+        logger.Log(logPrefix + "Constructor called");
+        sharding = 16;
+        executors = new Executor[sharding];
+        Integer total_rate = 0;
+        Integer rate = config.rate / sharding;
+        for(int i = 1; i < sharding; i++) {
+            executors[i] = new Executor(rate);
+            total_rate += rate;
+        }
+        executors[0] = new Executor(config.rate - total_rate);
+    }
+    public void Execute() {
+        logger.Log(logPrefix + "Execute called");
+        for(int i = 0; i < sharding; i++) {
+            executors[i].Execute();
+        }
+        logger.Log(logPrefix + "Execute end");
+    }
+    public void Stop() {
+        logger.Log(logPrefix + "Stop called");
+        for(int i = 0; i < sharding; i++) {
+            executors[i].Stop();
+        }
+        logger.Log(logPrefix + "Stop end");
+    }
+}
