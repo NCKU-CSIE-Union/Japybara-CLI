e85b227388b7cbe273821d12f7d3ae9b7292074f
HenryChang6 <henry930619@gmail.com>
2024-05-26 16:16:48 +0800

    Update Task.java

--- src/main/java/com/ncku_csie_union/Main.java
+++ src/main/java/com/ncku_csie_union/Main.java
@@ -1,26 +1,31 @@
 package com.ncku_csie_union;
 import com.ncku_csie_union.resources.Executor;
 import com.ncku_csie_union.resources.CommandLine;
+import com.ncku_csie_union.resources.Config;
+import com.ncku_csie_union.resources.Task;
 import com.ncku_csie_union.resources.Dispatcher;
 public class Main {
     public static void main(String[] args) throws Exception {
-        CommandLine commandLine = new CommandLine();
-        commandLine.Parse(args);
+        // CommandLine commandLine = new CommandLine();
+        // commandLine.Parse(args);
         System.out.println("Hello, World!");
+        Config config = Config.GetInstance();
+        config.uri = "https://www.youtube.com/";
+        Task task = new Task();
+        task.Execute();
+        // Dispatcher dispatcher = new Dispatcher();
+        // Runnable runnable = () -> {
+        //     try {
+        //         Thread.sleep(5000);
+        //         System.out.println("Try to stop executor");
+        //         dispatcher.Stop();
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+        // };
+        // Thread.startVirtualThread(runnable);
 
-        Dispatcher dispatcher = new Dispatcher();
-        Runnable runnable = () -> {
-            try {
-                Thread.sleep(5000);
-                System.out.println("Try to stop executor");
-                dispatcher.Stop();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
-        Thread.startVirtualThread(runnable);
-
-        dispatcher.Execute();
-        System.out.println("Main Thread Exit");
+        // dispatcher.Execute();
+        // System.out.println("Main Thread Exit");
     }
 }
--- src/main/java/com/ncku_csie_union/resources/Task.java
+++ src/main/java/com/ncku_csie_union/resources/Task.java
@@ -1,2 +1,61 @@
 package com.ncku_csie_union.resources;
 import com.ncku_csie_union.resources.interfaces.ITask;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.io.InputStream;
+
+public class Task implements ITask {
+    private URI uri = null;
+    private HttpURLConnection connection = null;
+    private long responseTime = -1;
+    private int dataSize = -1;
+    public Task() {
+        Config config = Config.GetInstance();
+        try {
+            uri = new URI(config.uri);
+        } catch (Exception e) {
+            System.err.println("Error: Invalid URI.");
+            // System.exit(1);
+        }
+    }
+    public void Execute() {
+        try {
+            this.connection = (HttpURLConnection) uri.toURL().openConnection();
+            this.connection.setRequestMethod("GET");
+
+            long startTime = System.currentTimeMillis();
+            int response_code = this.connection.getResponseCode();  // Triggers the request
+            long endTime = System.currentTimeMillis();
+
+            this.responseTime = endTime - startTime;
+
+            if(response_code == HttpURLConnection.HTTP_OK){
+                InputStream inputStream = connection.getInputStream();
+                byte[] data = new byte[1024];
+                int bytesRead = 0;
+                int totalBytesRead = 0;
+                while((bytesRead = inputStream.read(data, 0, data.length)) != -1)
+                    totalBytesRead += bytesRead;
+                this.dataSize = totalBytesRead;
+            } else {
+                System.out.println("For uri: " + uri.toString() + " occurs some HTTP error.");
+                System.err.println("Error code: " + response_code);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) 
+                connection.disconnect();
+            System.out.println("Response Time: " + responseTime + "ms");
+            System.out.println("Data Size: " + dataSize + " bytes");
+            
+        }
+    }
+    public void Stop() {
+        if (connection != null) 
+            connection.disconnect();
+        else 
+            System.out.println("Error: Connection is null.");
+    }
+}
