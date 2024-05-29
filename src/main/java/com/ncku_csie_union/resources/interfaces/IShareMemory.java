351eb6fee226bcd4a780affcd70e5aa7fc63ef06
peterxcli <peterxcli@gmail.com>
2024-05-29 14:39:48 +0800

    feat: add IResultCollector interface
    - Introduce IResultCollector interface with methods for appending reports and retrieving records.
    - Remove IShareMemory interface and its methods.

--- src/main/java/com/ncku_csie_union/resources/interfaces/IShareMemory.java
+++
@@ -1,6 +0,0 @@
-package com.ncku_csie_union.resources.interfaces;
-import com.ncku_csie_union.resources.interfaces.core.ILock;
-
-public interface IShareMemory {
-    public void appendReport(long duration, long dataReceived);
-}
