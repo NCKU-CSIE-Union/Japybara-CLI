3da795e33d85cfc4913bdebb783a21b98ba4f263
HenryChang6 <henry930619@gmail.com>
2024-05-31 18:21:07 +0800

    Recover main

--- src/main/java/com/ncku_csie_union/Main.java
+++ src/main/java/com/ncku_csie_union/Main.java
@@ -9,19 +9,19 @@ public class Main {
         CommandLine commandLine = new CommandLine();
         commandLine.Parse(args);
         System.out.println("Hello, World!");
-//        Dispatcher dispatcher = new Dispatcher();
-//        Runnable runnable = () -> {
-//            try {
-//                Thread.sleep(5000);
-//                System.out.println("Try to stop executor");
-//                dispatcher.Stop();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        };
-//        Thread.startVirtualThread(runnable);
-//
-//        dispatcher.Execute();
-//        System.out.println("Main Thread Exit");
+        Dispatcher dispatcher = new Dispatcher();
+        Runnable runnable = () -> {
+            try {
+                Thread.sleep(5000);
+                System.out.println("Try to stop executor");
+                dispatcher.Stop();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        Thread.startVirtualThread(runnable);
+
+        dispatcher.Execute();
+        System.out.println("Main Thread Exit");
     }
 }
