979eef304fa8b0fe9fbac5f4e283ca15febe452c
HenryChang6 <henry930619@gmail.com>
2024-05-31 14:43:17 +0800

    refactor commandline test

--- src/test/java/com/ncku_csie_union/resources/CommandLineTest.java
+++ src/test/java/com/ncku_csie_union/resources/CommandLineTest.java
@@ -2,63 +2,63 @@ package com.ncku_csie_union.resources;
 
 import com.ncku_csie_union.resources.interfaces.ICommandLine;
 import org.junit.Test;
+import org.junit.Before;
+
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 public class CommandLineTest {
+    private CommandLine cmd;
+    @Before
+    public void setUp() {
+        cmd = new CommandLine();
+    }
     @Test
     public void NoURIProvided() {
         String[] args = new String[] {"run"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals("non-receive", CommandLine.RunCommand.get_uri());
     }
     @Test
     public void NegativeRateResetTo100() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--rate", "-10"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(100, CommandLine.RunCommand.get_rate());
     }
     @Test
     public void ZeroRateResetTo100() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--rate", "0"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(100, CommandLine.RunCommand.get_rate());
     }
     @Test
     public void NormalVUNumber888() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--vu", "888"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(888, CommandLine.RunCommand.get_vu());
     }
     @Test
     public void EdgeVUNumber10000() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--vu", "10000"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(10000, CommandLine.RunCommand.get_vu());
     }
     @Test
     public void EdgeVUNumber10001() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--vu", "10001"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(500, CommandLine.RunCommand.get_vu());
     }
     @Test
     public void EdgeVUNumber1() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--vu", "1"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(1, CommandLine.RunCommand.get_vu());
     }
     @Test
     public void EdgeVUNumber0() {
         String[] args = {"run", "--uri", "https://www.google.com/", "--vu", "0"};
-        CommandLine cmd = new CommandLine();
         cmd.Parse(args);
         assertEquals(500, CommandLine.RunCommand.get_vu());
     }
