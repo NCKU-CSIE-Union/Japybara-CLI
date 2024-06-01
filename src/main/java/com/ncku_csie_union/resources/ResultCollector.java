d8e55daf92f7378a029b041aed158f19490ef27f
jason810496 <810496@email.wlsh.tyc.edu.tw>
2024-06-01 13:22:03 +0800

    refactor: use TaskRecord as ResultCollector's data transfer object

--- src/main/java/com/ncku_csie_union/resources/ResultCollector.java
+++ src/main/java/com/ncku_csie_union/resources/ResultCollector.java
@@ -10,8 +10,7 @@ import com.ncku_csie_union.resources.model.TaskRecord;
 public class ResultCollector implements IResultCollector {
     private ArrayList<TaskRecord> recordList = new ArrayList<TaskRecord>();
 
-    public void AppendReport(long duration, long dataReceived) {
-        TaskRecord record = new TaskRecord(duration, dataReceived);
+    public void AppendReport(TaskRecord record) {
         recordList.add(record);
     }
 
